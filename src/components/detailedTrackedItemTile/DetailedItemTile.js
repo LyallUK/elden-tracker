26b9ab27f32490bd2fdb5d40a661e5349b4336f4
David Lyall <lyalluk@outlook.com>
2024-01-19 13:58:40 +0000

    Added CSS to detailed tracking items

--- src/components/detailedTrackedItemTile/DetailedItemTile.js
+++ src/components/detailedTrackedItemTile/DetailedItemTile.js
@@ -1,37 +1,57 @@
 import React from "react";
 
-
 function DetailedItemTile(props) {
-
     const handleTrackItem = () => {
-        if(props.trackedIDList.includes(props.id)){
+        if (props.trackedIDList.includes(props.id)) {
             props.removeTrackedID(props.id);
-        }
-        else{
+        } else {
             props.addTrackedID(props.id);
         }
     };
 
     const handleCollectItem = () => {
-        if(props.collectedIDList.includes(props.id)){
+        if (props.collectedIDList.includes(props.id)) {
             props.removeCollectedID(props.id);
-        }
-        else{
+        } else {
             props.addCollectedID(props.id);
         }
-    }
-    
-    
-    return(
+    };
+
+    // Some locations return multiple text elements - see database for structure
+    // Create a function to generate a <p> element for each location
+
+    // const getLocations = () => {
+    //     props.itemLocation.forEach((location) => {
+    //         const locationWrapper = document.getElementsByClassName("detailed-item-content");
+
+    //     });
+    // };
+
+    return (
         <div className="detailed-item-wrapper">
-            <span className="detailed-item-name">{props.itemName}</span>
             <img className="detailed-item-image" src={props.itemImage} alt="detailed-Item"></img>
+            <div className="detailed-item-content">
+                <span className="detailed-item-name">{props.itemName}</span>
+                <p className="detailed-item-location">{props.itemLocation}</p>
+            </div>
             <div className="detailed-item-buttons">
-                <img className="item-button" id="track-button" src={props.serveTrackedIcon(props.id)} onClick={handleTrackItem} alt="+"></img>
-                <img className="item-button" id="collected-button" src={props.serveCollectedIcon(props.id)} onClick={handleCollectItem} alt="c"></img>
+                <img
+                    className="detailed-item-button"
+                    id="track-button"
+                    src={props.serveTrackedIcon(props.id)}
+                    onClick={handleTrackItem}
+                    alt="+"
+                ></img>
+                <img
+                    className="detailed-item-button"
+                    id="collected-button"
+                    src={props.serveCollectedIcon(props.id)}
+                    onClick={handleCollectItem}
+                    alt="c"
+                ></img>
             </div>
         </div>
-    )
+    );
 }
 
 export default DetailedItemTile;
